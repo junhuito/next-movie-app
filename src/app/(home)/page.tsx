40e9cb74af3e5d6e24e90b7921379795ba09c610
Jun <junhuito5771@gmail.com>
2024-12-21 01:50:50 +0800

    Simple hook

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,64 @@
-import { Skeleton, Button, Typography } from '@mui/material';
+"use client";
+import { Container, Button, Typography, Input, FormLabel } from '@mui/material';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+
+interface Todo {
+  completed: boolean;
+  id: number;
+  title: string;
+  userId: number;
+}
 
 export default function Home() {
+
+  const [data, setData] = useState<Todo>();
+  const [todoId, setTodoId] = useState<number>(1);
+
+  useEffect(() => {
+    if (todoId) {
+      fetch(`https://jsonplaceholder.typicode.com/todos/${todoId}`)
+        .then(response => response.json())
+        .then(json => setData(json))
+    }
+  }, [todoId])
+
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  const reset = useCallback(() => {
+    setA(0);
+    setB(0);
+  }, [])
+
+  const sumResult = useMemo(() => a + b, [a, b])
+
   return (
-    <>
-      <Skeleton variant="rectangular" width={210} height={118} />
-      <Button variant="contained">Hello world</Button>
-      <Typography variant="h1">Hello world</Typography>
-    </>
+    <Container className='space-y-4 p-10'>
+      <Container>
+        <FormLabel>Enter A:</FormLabel>
+        <Input value={a} onChange={(e) => setA(Number(e.target.value))}/>
+      </Container>
+
+      <Container>
+        <FormLabel>Enter B:</FormLabel>
+        <Input value={b} onChange={(e) => setB(Number(e.target.value))}/>
+      </Container>
+      
+      <Button variant="contained" onClick={reset}>Reset</Button>
+      <Typography>Result: {a} + {b} = {sumResult}</Typography>
+
+      <Container>
+        <FormLabel>Enter Todo ID:</FormLabel>
+        <Input value={todoId} onChange={(e) => setTodoId(Number(e.target.value))}/>
+      </Container>
+
+      <Container>
+        <Typography variant="h4">Data from API</Typography>
+        <Typography>ID: {data?.id}</Typography>
+        <Typography>Title: {data?.title}</Typography>
+        <Typography>Completed: {data?.completed ? 'Yes' : 'No'}</Typography>
+        <Typography>User ID: {data?.userId}</Typography>
+      </Container>
+    </Container>
   );
 }
