bd9071f0437c51fea5e9eb61aa9e0318d29fa6db
Jun <junhuito5771@gmail.com>
2024-12-31 12:30:24 +0800

    scroll behavior

--- src/components/MovieCarousel.tsx
+++ src/components/MovieCarousel.tsx
@@ -1,34 +1,3 @@
-// import { Box, Skeleton } from "@mui/material";
-// import { MovieCard } from "./MovieCard";
-// import { Movie } from "@/types/movie";
-
-// export const MovieCarousel = ({ movies }: { movies?: Movie[] }) => {
-//   if (!movies) {
-//     return (
-//       <Box component="div" className="flex space-x-4 overflow-hidden">
-//         {Array.from({ length: 10 })?.map((_, i) => (
-//           <Skeleton
-//             className="block basis-60 shrink-0"
-//             variant="rounded"
-//             key={i}
-//             width={200}
-//             height={150}
-//           />
-//         ))}
-//       </Box>
-//     );
-//   }
-
-//   return (
-//     <Box component="div" className="flex space-x-4 overflow-auto">
-//       {movies?.map((movie) => (
-//         <MovieCard key={movie._id} data={movie} />
-//       ))}
-//     </Box>
-//   );
-// };
-
-
 import { Box, Skeleton } from "@mui/material";
 import { MovieCard } from "./MovieCard";
 import { Movie } from "@/types/movie";
@@ -51,10 +20,10 @@ export const MovieCarousel = ({ movies }: { movies?: Movie[] }) => {
   }
 
   return (
-    <Box component="div" className="flex space-x-4 overflow-auto">
+    <Box component="div" className="flex space-x-4 overflow-auto snap-x snap-mandatory">
       {movies?.map((movie) => (
-        <Box key={movie._id} component="div" className="overflow-hidden rounded-2xl flex-shrink-0 basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/5 h-auto">
-            <MovieCard data={movie} />
+        <Box key={movie._id} component="div" className="snap-start overflow-hidden rounded-2xl flex-shrink-0 basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/5 h-auto">
+          <MovieCard data={movie} />
         </Box>
       ))}
     </Box>
